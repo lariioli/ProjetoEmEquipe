837c63bb742abe77dcadd68e06ca1cb284ad967d
Bruna <cristalbrumara@gmail.com>
2024-09-23 14:05:44 -0300

    codigo criado

--- Geradordesenha/Program.cs
+++ Geradordesenha/Program.cs
@@ -1,5 +1,4 @@
-# ProjetoEmEquipe
-string[] opcoes = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+﻿string[] opcoes = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         Random random = new Random();
 
         Console.WriteLine("Qual o tamanho da senha?");
