45eeb5a9245df57c9dedaf42587556bf9c7795b4
arthrs7 <marcosviniciusdiasalmeida@gmail.com>
2024-09-24 13:37:20 -0300

    codigo atualizado

--- CalculadoraDeIMC/Program.cs
+++ CalculadoraDeIMC/Program.cs
@@ -16,19 +16,19 @@ public class Program
         
         if (imc < 18.5)
         {
-            Console.WriteLine("Categoria: Abaixo do peso");
+            Console.WriteLine("Categoria: Abaixo do peso.");
         }
         else if (imc >= 18.5 && imc < 24.9)
         {
-            Console.WriteLine("Categoria: Peso normal");
+            Console.WriteLine("Categoria: Peso normal.");
         }
         else if (imc >= 25 && imc < 29.9)
         {
-            Console.WriteLine("Categoria: Sobrepeso");
+            Console.WriteLine("Categoria: Sobrepeso.");
         }
         else
         {
-            Console.WriteLine("Categoria: Obesidade");
+            Console.WriteLine("Categoria: Obesidade.");
         }
     }
 }
